3f4951aeb50eecb0ae88c9f11f20b55c7151e32f
mahmud <mahmudayaz2024@gmail.com>
2025-09-05 21:11:47 +0600

    first commit

---
+++ lib/ui/utils/asset_paths.dart
@@ -0,0 +1,7 @@
+class AssetPaths{
+  static const _imagePath = 'assets/images';
+  static const backgroundSvg = '$_imagePath/background.svg';
+  static const logoSvg = '$_imagePath/logo.svg';
+
+
+}
